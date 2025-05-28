9d0ca849802c399f408acd9f5bd4767791ed7caa
manokhin_ms <manokhin_ms@directum.ru>
2025-05-29 00:03:04 +0400

    Add diagnostics to build.bat and release workflow, fix binary paths

--- build.bat
+++ build.bat
@@ -1,29 +1,50 @@
 @echo off
 pushd "%~dp0"
 
-set GOOS=windows
-set GO_BUILD_CMD=go build -o "./bin" -a -gcflags=all="-l -B" -ldflags="-w -s" -trimpath -buildvcs=false
+:: Создаём папку bin
+echo Creating bin directory...
+rmdir bin /S /Q
+mkdir bin
+if %errorlevel% neq 0 (
+  echo Failed to create bin directory
+  popd
+  exit /b %errorlevel%
+)
 
-mkdir -p bin
+set GOOS=windows
+set GO_BUILD_CMD=go build -o "bin/gosnap.exe" -a -gcflags=all="-l -B" -ldflags="-w -s" -trimpath -buildvcs=false
+
 echo %GOOS% build ...
 echo %GO_BUILD_CMD%
 %GO_BUILD_CMD%
 if %errorlevel% neq 0 (
+  echo Build for %GOOS% failed
   popd
-  echo exit code: %errorlevel%
   exit /b %errorlevel%
+)
+if not exist "bin\gosnap.exe" (
+  echo Error: bin/gosnap.exe not created
+  popd
+  exit /b 1
 )
 
 set GOOS=linux
+set GO_BUILD_CMD=go build -o "bin/gosnap" -a -gcflags=all="-l -B" -ldflags="-w -s" -trimpath -buildvcs=false
+
 echo %GOOS% build ...
 echo %GO_BUILD_CMD%
 %GO_BUILD_CMD%
 if %errorlevel% neq 0 (
+  echo Build for %GOOS% failed
   popd
-  echo exit code: %errorlevel%
   exit /b %errorlevel%
+)
+if not exist "bin\gosnap" (
+  echo Error: bin/gosnap not created
+  popd
+  exit /b 1
 )
 
 popd
-echo well done.
+echo Well done.
 exit /b %errorlevel%
